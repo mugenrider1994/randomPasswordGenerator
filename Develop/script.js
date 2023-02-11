c7e0545a79bfe988057365e1c74378a62a8d7de0
Megan Glorioso <meganglorioso2016@gmail.com>
2023-02-10 19:28:42 -0700

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
 var specialCharacters = ("");
-var upperCase = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
-var lowerCase = [abcdefghijklmnopqrstuvwxyz];
-var numbers = [1234567890];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
 var minNumberOfChars = 8;
 var maxNumberOfChars = 128;
 
